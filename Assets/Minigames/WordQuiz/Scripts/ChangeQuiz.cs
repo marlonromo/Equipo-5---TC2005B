d78c38676428718794a6074872b3a92c5891efc7
NicolasHerreraHdez <a01114972@itesm.mx>
2021-10-20 17:45:02 -0600

    Se añadio menu de quiz

---
+++ Assets/Minigames/WordQuiz/Scripts/ChangeQuiz.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeQuiz : MonoBehaviour
+{
+    public GameObject choiceBtn;
+    public GameObject wordBtn;
+
+
+    public void ChoiceBtn()
+    {
+        SceneManager.LoadScene("ChoiceQuiz");
+    }
+
+    public void WordBtn()
+    {
+        SceneManager.LoadScene("WordQuiz");
+    }
+}
